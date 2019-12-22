3f957a41629cec49ab918a826491d305c3ad0944
Franklin Rivero <frrivero@outlook.com>
2019-12-22 10:28:21 -0600

    Create Snapshot command added

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -6,8 +6,23 @@ ec2 = session.resource('ec2')
 
 
 @click.group()
+def cli():
+    """Shotty CLI commands"""
+
+
+@cli.group('volumes')
+def volumes():
+    """Commands for volumes"""
+
+
+@cli.group('instances')
 def instances():
     """Commands for instances"""
+
+
+@cli.group('snapshots')
+def snapshots():
+    """Commands for snapshots"""
 
 
 @instances.command('list')
@@ -35,10 +50,9 @@ def list_instances(project):
 def get_instances(project):
     if project:
         filters = [{'Name': 'tag:Project', 'Values': [project]}]
-        instances = ec2.instances.filter(Filters=filters)
-    else:
-        instances = ec2.instances.all()
-    return instances
+        return ec2.instances.filter(Filters=filters)
+
+    return ec2.instances.all()
 
 
 @instances.command('stop')
@@ -63,5 +77,74 @@ def stop_instances(project):
     return
 
 
+def get_volumes(project):
+    if project:
+        filters = [{'Name': 'tag:Project', 'Values': [project]}]
+        return ec2.volumes.filter(Filters=filters)
+
+    return ec2.volumes.all()
+
+
+@volumes.command('list')
+@click.option('--project', default=None, help='Only volumes for project')
+def list_volumes(project):
+    """List Volumes"""
+    for i in get_volumes(project):
+        print(', '.join((
+            i.id,
+            str(i.size) + "GB",
+            i.state,
+            ("Not_Encrypted", "Encrypted")[i.encrypted],  # (if_test_is_false, if_test_is_true)[test]
+            i.encrypted and "Encrypted" or "Not_Encrypted",
+        )))
+    return
+
+
+def get_snapshots(project):
+    filters = project and [{'Name': 'tag:Project', 'Values': [project]}] or []
+    result = []
+    for v in get_volumes(project):
+        for s in v.snapshots.filter(Filters=filters):
+            result.append(s)
+    return result
+
+
+@snapshots.command('list')
+@click.option('--project', default=None, help='Only snapshots for project')
+def list_snapshots(project):
+    """List Volume Snapshots"""
+    for i in get_snapshots(project):
+        # print(i)
+        print(', '.join((
+            i.id,
+            str(i.volume_size) + "GB",
+            i.state,
+            i.progress,
+            ("Not_Encrypted", "Encrypted")[i.encrypted],  # (if_test_is_false, if_test_is_true)[test]
+            i.encrypted and "Encrypted" or "Not_Encrypted",
+        )))
+    return
+
+
+@snapshots.command('create')
+@click.option('--project', default=None, help='Only snapshots for project')
+def create_snapshots(project):
+    """List Volume Snapshots"""
+    filters = project and [{'Name': 'tag:Project', 'Values': [project]}] or []
+    for i in get_instances(project):
+        i.stop()
+        print("Stopping instance: {0}".format(i.id))
+        i.wait_until_stopped()
+        for v in i.volumes.filter(Filters=filters):
+            print("     Creating snapshot for volume {0} of instance: {1}".format(v.id, i.id))
+            v.create_snapshot(Description='Created with Shotty')
+        i.start()
+        print("Starting instance: {0}".format(i.id))
+        i.wait_until_running()
+        print("Instance {0} started".format(i.id))
+    print("Job is done!!!")
+    return
+
+
 if __name__ == '__main__':
-    instances()
+    cli()
